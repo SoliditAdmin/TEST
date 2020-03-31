af6c789746bb459146e6424ba05a87b43738b1e8
yunasim <ynsim@solidit.co.kr>
2020-03-31 10:42:09 +0900

    업로드
    테스트용으로 만든 프로젝트를 폴더에  붙여넣어봄.

---
+++ Edrawing_TEST/Form1.cs
@@ -0,0 +1,46 @@
+﻿using eDrawingHostControl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edrawing_TEST
+{
+    public partial class Form1 : Form
+    {
+        eDrawingHostControl.eDrawingControl ctrl = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (null == ctrl)
+            {
+                ctrl = new eDrawingControl();
+            }
+            this.Controls.Add(ctrl);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ctrl != null)
+            {
+                pictureBox1.Visible = false;
+                //ctrl.Location = new Point(0, 0);
+                //ctrl.Size = new System.Drawing.Size(this.Size.Width, this.Size.Height);
+                ctrl.Location = pictureBox1.Location;
+                ctrl.Size = pictureBox1.Size;
+                ctrl.eDrawingControlWrapper.OpenDoc(@"C:\Users\HP\Desktop\DMS TEST File\4040 Profile.sldprt", false, false, false, "");
+            }
+        }
+    }
+}
